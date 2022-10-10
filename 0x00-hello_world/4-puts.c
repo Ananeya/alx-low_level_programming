95611437dc689d4206a41a1af917e6b2011a4079
Ananeya <ananeyap@gmail.com>
2022-10-09 21:05:44 -0700

    all the tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "programming is like buliding a multilingual puzzle
+ * return: 0
+ */ 
+int main(void)
+{
+	puts("\"programming is like bulding multilingual puzzle");
+	return (0);
+}
+
